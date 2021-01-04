0022de72f64d8048ffce503e9b1aa58f3b829242
Adam Hamar <adam.hamar@gmail.com>
2021-01-04 15:06:06 +0100

    Test the Range class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
+
 public class Main {
     public static void main(String[] args) {
-    
+        Range range1 = new Range(4);
+        System.out.printf("Start, end, length: %s, %s, %s%n", range1.getStart(), range1.getEnd(), range1.length());
+        Range range2 = new Range(2);
+        System.out.printf("Start, end, length: %s, %s, %s%n", range2.getStart(), range2.getEnd(), range2.length());
+        
     }
 }
